a100466a8e7933331853e32e3b4030e34785c9f5
ijyoon <hello@yalsooni.name>
2017-04-13 13:39:32 +0900

    011. 실행 메소드 파라미터 검증 추가

--- 011.Proxy/src/main/java/name/yalsooni/genius/proxy/execute/ProxyServer.java
+++ 011.Proxy/src/main/java/name/yalsooni/genius/proxy/execute/ProxyServer.java
@@ -5,16 +5,18 @@ import name.yalsooni.genius.common.definition.Service;
 import name.yalsooni.genius.common.definition.annotation.Delegate;
 import name.yalsooni.genius.common.definition.annotation.Entry;
 import name.yalsooni.genius.proxy.io.Proxy;
+import name.yalsooni.genius.util.Log;
 
 /**
- * proxy server.
+ * 011 proxy server.
  * Created by ijyoon on 2017. 4. 12..
  */
 @Delegate(projectID = "011", serviceType = Service.RUNTIME)
 public class ProxyServer {
 
     /**
-     * io 방식의 프록시 서버
+     * 지니어스 어노테이션.
+     * 011 : io 방식의 프록시 서버
      * @param sourcePort
      * @param targetIP
      * @param targetPort
@@ -30,8 +32,21 @@ public class ProxyServer {
         proxy.execute();
     }
 
+    /**
+     * 실행
+     * @param args
+     */
     public static void main(String[] args){
+
+        if(args.length !=3){
+            Log.console("파라미터 값을 입력하세요.");
+            Log.console("1. listen port");
+            Log.console("2. target IP");
+            Log.console("3. target Port");
+            return;
+        }
+
         ProxyServer ps = new ProxyServer();
-        ps.ioBind(7070, "localhost", 8080);
+        ps.ioBind(Integer.parseInt(args[0]),args[1],Integer.parseInt(args[2]));
     }
 }
